9b9820ecccab446640e969aae414272ca148d256
Marko <marko@farmeron.com>
2014-09-23 20:42:10 +0200

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuget_testing_v1
+{
+    public class Class1
+    {
+		public void Testing() { }
+    }
+}
